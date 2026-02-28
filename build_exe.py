75651ed90b63592658eca72c149d10ee722bd859
A-LAN-08 <adlanecki@outlook.com>
2026-02-28 15:01:26 +0000

    Not make pyinstaller run as admin

---
+++ build_exe.py
@@ -0,0 +1,29 @@
+
+import os
+import subprocess
+import shutil
+
+def main(config, out_name, exe_path):
+    apps = config.get('Settings', 'Apps', fallback='')
+    apps_list = str([app.strip().lower() for app in apps.split(',')])
+    password = config.get('Settings', 'Password', fallback='')
+
+    with open("main.py", "r") as f:
+        content = f.read()
+
+    content = content.replace('["REPLACE_APPS"]', apps_list)
+    content = content.replace('"REPLACE_PASSWORD"', f'"{password}"')
+
+    with open("temp.py", "w") as f:
+        f.write(content)
+
+    subprocess.run([
+        "pyinstaller",
+        "--onefile",
+        "--noconsole",
+        "--log-level", "WARN",
+        f"--name={out_name}",
+        "temp.py"
+    ])
+
+    shutil.copy(os.path.join("dist", f"{out_name}.exe"), exe_path)
---
+++ make_schedule.py
@@ -0,0 +1,63 @@
+
+import ctypes
+import sys
+import os
+import subprocess
+
+def main(out_name, exe_path):
+    admin_check()
+
+    ps_script = f"""
+    $action = New-ScheduledTaskAction -Execute '{exe_path}'
+    $trigger = New-ScheduledTaskTrigger -AtLogon
+    $principal = New-ScheduledTaskPrincipal -GroupId "S-1-5-32-545" -RunLevel Highest
+    $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries
+
+    Register-ScheduledTask -TaskName "{out_name}" -Action $action -Trigger $trigger -Principal $principal -Settings $settings -Force
+    """
+
+    result = subprocess.run(
+        ["powershell", "-NoProfile", "-Command", ps_script],
+        capture_output=True,
+        text=True
+    )
+
+    if result.returncode == 0:
+        print("PowerShell: Task registered successfully.")
+    else:
+        print(f"PowerShell Error: {result.stderr}")
+
+
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+def request_admin_privileges():
+    try:
+        script_path = os.path.abspath(sys.argv[0])
+        params = " ".join([f'"{arg}"' for arg in sys.argv[1:]])
+
+        result = ctypes.windll.shell32.ShellExecuteW(
+            None, "runas", sys.executable, f'"{script_path}" {params}', None, 1
+        )
+        return result > 32  # Success if result > 32
+    except Exception as e:
+        print("Error:", str(e))
+        return False
+
+def admin_check():
+    if not is_admin():
+        success = request_admin_privileges()
+        if success:
+            print("Requested admin privileges. Relaunching...")
+            sys.exit(0)
+        else:
+            print("Admin privilege request was denied.")
+            input("Press enter to exit...")
+            sys.exit(1)
+
+    else:
+        print("Running with admin privileges!")
+
